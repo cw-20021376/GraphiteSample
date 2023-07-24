8040f28bb28701a94208e319c6ec2c2604c90c0c
James <jamesLee@coway.com>
2023-07-24 13:23:37 +0900

    init commit

---
+++ GraphiteSampleProject/GraphiteSampleProject/GraphiteSampleProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  GraphiteSampleProjectApp.swift
+//  GraphiteSampleProject
+//
+//  Created by cw-james on 2023/07/24.
+//
+
+import SwiftUI
+
+@main
+struct GraphiteSampleProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
